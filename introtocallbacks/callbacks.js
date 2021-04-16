70335b447390d7e1ae2a7e4f9b38defc9d1b148b
Philip Lowe <philip.lowe94@gmail.com>
2021-04-16 16:52:15 -0400

    myThrottle

--- introtocallbacks/callbacks.js
+++ introtocallbacks/callbacks.js
@@ -1,117 +1,117 @@
 // class Clock {
-//   constructor() {
-//     // 1. Create a Date object.
-//     let date = new Date();
-//     // 2. Store the hours, minutes, and seconds.
-//     this.hours = date.getHours();
-//     this.minutes = date.getMinutes();
-//     this.seconds = date.getSeconds();
-//     // 3. Call printTime.
-//     this.printTime();
-//     // 4. Schedule the tick at 1 second intervals.
-//     this._tick();
-//   }
+// //   constructor() {
+// //     // 1. Create a Date object.
+// //     let date = new Date();
+// //     // 2. Store the hours, minutes, and seconds.
+// //     this.hours = date.getHours();
+// //     this.minutes = date.getMinutes();
+// //     this.seconds = date.getSeconds();
+// //     // 3. Call printTime.
+// //     this.printTime();
+// //     // 4. Schedule the tick at 1 second intervals.
+// //     this._tick();
+// //   }
 
 
 
-//   printTime() {
-//     // Format the time in HH:MM:SS
-//     let time = `${this.hours}:${this.minutes}:${this.seconds}`;
-//     // Use console.log to print it.
-//     console.log(time);
-//   };
+// //   printTime() {
+// //     // Format the time in HH:MM:SS
+// //     let time = `${this.hours}:${this.minutes}:${this.seconds}`;
+// //     // Use console.log to print it.
+// //     console.log(time);
+// //   };
 
-//   _tick() {
-//     // 1. Increment the time by one second.
-//     // 2. Call printTime.
-//   setTimeout(() => {
-//       this.seconds += 1;
-//       if (this.seconds === 60){
-//         this.seconds = 0;
-//         this.minutes += 1;
-//         if (this.minutes === 60) {
-//           this.minutes = 0;
-//           this.hours += 1
-//           if (this.hours === 24) {
-//             this.hours = 1;
-//           };
-//         };
-//       };
+// //   _tick() {
+// //     // 1. Increment the time by one second.
+// //     // 2. Call printTime.
+// //   setTimeout(() => {
+// //       this.seconds += 1;
+// //       if (this.seconds === 60){
+// //         this.seconds = 0;
+// //         this.minutes += 1;
+// //         if (this.minutes === 60) {
+// //           this.minutes = 0;
+// //           this.hours += 1
+// //           if (this.hours === 24) {
+// //             this.hours = 1;
+// //           };
+// //         };
+// //       };
     
-//       this.printTime();
-//       this._tick();
-//     } , 1000);}
+// //       this.printTime();
+// //       this._tick();
+// //     } , 1000);}
+// // }
+
+
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
+
+// function addNumbers(sum, numsLeft, completionCallback) {
+//   if (numsLeft === 0) {
+//     completionCallback(sum);
+//   };
+//   if (numsLeft > 0) {
+//     const response = rl.question('Type a number', num => {
+//       sum += parseInt(num);
+//       console.log(sum);
+//       addNumbers(sum, numsLeft - 1, completionCallback);
+//     });
+//   };
 // }
 
+// // addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
 
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// //absurdBubbleSort
 
-function addNumbers(sum, numsLeft, completionCallback) {
-  if (numsLeft === 0) {
-    completionCallback(sum);
-  };
-  if (numsLeft > 0) {
-    const response = rl.question('Type a number', num => {
-      sum += parseInt(num);
-      console.log(sum);
-      addNumbers(sum, numsLeft - 1, completionCallback);
-    });
-  };
-}
+// function askIfGreaterThan(el1, el2, callback) {
+//   const response = rl.question(`Is ${el1} greater than ${el2}? Type 'yes' or 'no'. `, ans => {
+//     if (ans === "yes") {
+//       callback(true);
+//     } else {
+//       callback(false);
+//     }
+//   });
+// };
 
-// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+// // askIfGreaterThan(7,6, (el) => console.log(el));
 
-//absurdBubbleSort
+// function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+//   if (i < arr.length - 1) {
+//     askIfGreaterThan(arr[i], arr[i+1], (bool) => {
+//       if (bool) {
+//         let arr_swap = arr[i];
+//         arr[i] = arr[i + 1];
+//         arr[i + 1] = arr_swap;
+//         // arr[i], arr[i+1] = arr[i+1], arr[i];
+//         madeAnySwaps = true;
+//       }
+//       innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop)
+//     });
+//   }
+//   if (i == (arr.length -1)) {
+//     outerBubbleSortLoop(madeAnySwaps);
+//     // rl.close();
+//   }
+// }
 
-function askIfGreaterThan(el1, el2, callback) {
-  const response = rl.question(`Is ${el1} greater than ${el2}? Type 'yes' or 'no'. `, ans => {
-    if (ans === "yes") {
-      callback(true);
-    } else {
-      callback(false);
-    }
-  });
-};
+// // innerBubbleSortLoop([2,1,3], 0, false, () => console.log('In outer bubble sort'))
 
-// askIfGreaterThan(7,6, (el) => console.log(el));
+// function absurdBubbleSort(arr, sortCompletionCallback) {
+//   function outerBubbleSortLoop(madeAnySwaps) {
+//     if (madeAnySwaps) {
+//       innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+//     } else {
+//       sortCompletionCallback(arr);
+//     };
+//   }
 
-function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
-  if (i < arr.length - 1) {
-    askIfGreaterThan(arr[i], arr[i+1], (bool) => {
-      if (bool) {
-        let arr_swap = arr[i];
-        arr[i] = arr[i + 1];
-        arr[i + 1] = arr_swap;
-        // arr[i], arr[i+1] = arr[i+1], arr[i];
-        madeAnySwaps = true;
-      }
-      innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop)
-    });
-  }
-  if (i == (arr.length -1)) {
-    outerBubbleSortLoop(madeAnySwaps);
-    // rl.close();
-  }
-}
+//   outerBubbleSortLoop(true);
 
-// innerBubbleSortLoop([2,1,3], 0, false, () => console.log('In outer bubble sort'))
-
-function absurdBubbleSort(arr, sortCompletionCallback) {
-  function outerBubbleSortLoop(madeAnySwaps) {
-    if (madeAnySwaps) {
-      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
-    } else {
-      sortCompletionCallback(arr);
-    };
-  }
-
-  outerBubbleSortLoop(true);
-
-}
+// }
 
 
 
@@ -157,29 +157,58 @@ function absurdBubbleSort(arr, sortCompletionCallback) {
 // });
 
 
-Function.prototype.myBind = function(context) {
-  return () => {
-    this.apply(context);
-  }
-};
+// Function.prototype.myBind = function(context) {
+//   return () => {
+//     this.apply(context);
+//   }
+// };
 
-class Lamp {
-  constructor() {
-    this.name = "a lamp";
+
+// class Lamp {
+//   constructor() {
+//     this.name = "a lamp";
+//   }
+// }
+
+// const turnOn = function() {
+//   console.log("Turning on " + this.name);
+// };
+
+// const lamp = new Lamp();
+
+// turnOn(); // should not work the way we want it to
+
+// const boundTurnOn = turnOn.bind(lamp);
+// const myBoundTurnOn = turnOn.myBind(lamp);
+
+// boundTurnOn(); // should say "Turning on a lamp"
+// myBoundTurnOn(); // should say "Turning on a lamp"
+
+/////////////// my Throttle
+
+Function.prototype.myThrottle = function(interval) {
+  let tooSoon = false;
+  return (...args) => {
+
+    if (!tooSoon) {
+
+      tooSoon = true;
+
+      setTimeout( () => {
+        tooSoon = false;
+      }, interval );
+
+      this(...args);
+    }
   }
 }
 
-const turnOn = function() {
-  console.log("Turning on " + this.name);
-};
+class Neuron {
+  constructor() {
+    this.fire = this.fire.myThrottle(500);
+  }
 
-const lamp = new Lamp();
-
-turnOn(); // should not work the way we want it to
-
-const boundTurnOn = turnOn.bind(lamp);
-const myBoundTurnOn = turnOn.myBind(lamp);
-
-boundTurnOn(); // should say "Turning on a lamp"
-myBoundTurnOn(); // should say "Turning on a lamp"
-
+  fire(str) {
+    console.log(`Firing! ${str}`);
+  }
+}
