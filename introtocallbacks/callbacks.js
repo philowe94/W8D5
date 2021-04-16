471b7280d752b2595d5a15694f17f6de6a68d1c3
Philip Lowe <philip.lowe94@gmail.com>
2021-04-16 15:36:03 -0400

    innerbubblesortloop

--- introtocallbacks/callbacks.js
+++ introtocallbacks/callbacks.js
@@ -1,47 +1,47 @@
-class Clock {
-  constructor() {
-    // 1. Create a Date object.
-    let date = new Date();
-    // 2. Store the hours, minutes, and seconds.
-    this.hours = date.getHours();
-    this.minutes = date.getMinutes();
-    this.seconds = date.getSeconds();
-    // 3. Call printTime.
-    this.printTime();
-    // 4. Schedule the tick at 1 second intervals.
-    this._tick();
-  }
+// class Clock {
+//   constructor() {
+//     // 1. Create a Date object.
+//     let date = new Date();
+//     // 2. Store the hours, minutes, and seconds.
+//     this.hours = date.getHours();
+//     this.minutes = date.getMinutes();
+//     this.seconds = date.getSeconds();
+//     // 3. Call printTime.
+//     this.printTime();
+//     // 4. Schedule the tick at 1 second intervals.
+//     this._tick();
+//   }
 
 
 
-  printTime() {
-    // Format the time in HH:MM:SS
-    let time = `${this.hours}:${this.minutes}:${this.seconds}`;
-    // Use console.log to print it.
-    console.log(time);
-  };
+//   printTime() {
+//     // Format the time in HH:MM:SS
+//     let time = `${this.hours}:${this.minutes}:${this.seconds}`;
+//     // Use console.log to print it.
+//     console.log(time);
+//   };
 
-  _tick() {
-    // 1. Increment the time by one second.
-    // 2. Call printTime.
-  setTimeout(() => {
-      this.seconds += 1;
-      if (this.seconds === 60){
-        this.seconds = 0;
-        this.minutes += 1;
-        if (this.minutes === 60) {
-          this.minutes = 0;
-          this.hours += 1
-          if (this.hours === 24) {
-            this.hours = 1;
-          };
-        };
-      };
+//   _tick() {
+//     // 1. Increment the time by one second.
+//     // 2. Call printTime.
+//   setTimeout(() => {
+//       this.seconds += 1;
+//       if (this.seconds === 60){
+//         this.seconds = 0;
+//         this.minutes += 1;
+//         if (this.minutes === 60) {
+//           this.minutes = 0;
+//           this.hours += 1
+//           if (this.hours === 24) {
+//             this.hours = 1;
+//           };
+//         };
+//       };
     
-      this.printTime();
-      this._tick();
-    } , 1000);}
-}
+//       this.printTime();
+//       this._tick();
+//     } , 1000);}
+// }
 
 
 const readline = require("readline");
@@ -50,26 +50,53 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-
 function addNumbers(sum, numsLeft, completionCallback) {
   if (numsLeft === 0) {
     completionCallback(sum);
-  }
+  };
   if (numsLeft > 0) {
     const response = rl.question('Type a number', num => {
-      sum += parseInt('num');
+      sum += parseInt(num);
       console.log(sum);
       addNumbers(sum, numsLeft - 1, completionCallback);
-    })
+    });
+  };
+}
+
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+//absurdBubbleSort
+
+function askIfGreaterThan(el1, el2, callback) {
+  const response = rl.question(`Is ${el1} greater than ${el2}? Type 'yes' or 'no'. `, ans => {
+    if (ans === "yes") {
+      callback(true);
+    } else {
+      callback(false);
+    }
+  });
+};
+
+// askIfGreaterThan(7,6, (el) => console.log(el));
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  if (i < arr.length - 1) {
+    askIfGreaterThan(arr[i], arr[i+1], (bool) => {
+      if (bool) {
+        arr[i], arr[i+1] = arr[i+1], arr[i];
+        madeAnySwaps = true;
+      }
+      innerBubbleSortLoop(arr, i+1, madeAnySwaps, outerBubbleSortLoop)
+    });
   }
+  if (i == (arr.length -1)) {
+    outerBubbleSortLoop(madeAnySwaps);
+    rl.close();
+  }
+}
 
+innerBubbleSortLoop([2,1,3], 0, false, () => console.log('In outer bubble sort'))
 
+function absurdBubbleSort(arr, sortCompletionCallback) {
 
-
-  // let partialSum = 0;
-  // for (let i = 0; i < nums.length; i++) {
-  //   console.log(partialSum);
-  //   partialSum += nums[i]
-  // }
-  // return partialSum;
 }
